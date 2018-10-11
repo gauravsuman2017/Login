6d6182707aa22c2b47c6f1b11a7e4b7359a5e60b
gaurav suman <gauravsuman.vits@gmail.com>
2018-10-11 23:22:16 +0530

    added files

---
+++ src/add/Add.java
@@ -0,0 +1,31 @@
+
+package add;
+
+/**
+ *
+ * @author gaurav
+ */
+public class Add {
+
+
+
+  
+     
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int i = 5, j = 15, sum;
+      sum = i + j;
+if(i==5|| false)
+
+      System.out.println("hh");
+else
+        System.out.println("False");
+    
+    
+}
+}
